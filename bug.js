a555c9c00781fc4d7ffbaf51820c846d8b85885c
BugHunter-X <askzenai@gmail.com>
2025-01-19 20:40:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,29 @@
+```javascript
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* This route will cause the error because it's a nested route without a parent route */}
+        <Route path="/users/:userId/profile" element={<UserProfile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return <div>Home</div>;
+}
+
+function About() {
+  return <div>About</div>;
+}
+
+function UserProfile() {
+  return <div>User Profile</div>;
+}
+export default App;
+```
